0a57da1d5d3d6946799b31df3dd8bca1c308ae48
Erik Gafni <egafni@gmail.com>
2013-04-24 10:16:01 -0400

    update orch settings

--- genomekey/wga_settings.py
+++ genomekey/wga_settings.py
@@ -15,7 +15,7 @@ if settings['server_name'] == 'gpp':
         'get_drmaa_native_specification':session.default_get_drmaa_native_specification
         }
 
-elif settings['server_name']  == 'orchestra':
+elif settings['server_name']  in ['orchestra','orchestra2'] :
     def get_drmaa_native_specification(jobAttempt):
         task = jobAttempt.task
         DRM = settings['DRM']
@@ -42,13 +42,18 @@ elif settings['server_name']  == 'orchestra':
         else:
             raise Exception('DRM not supported')
 
-    resource_bundle_path = '/groups/lpm/erik/WGA/bundle/2.2/b37'
-    tools_dir = '/groups/lpm/erik/WGA/tools'
+    if 'server_name' == 'orchestra':
+        resource_bundle_path = '/groups/cbi/WGA/bundle/2.2/b37'
+        tools_dir = '/groups/cbi/WGA/tools'
+    elif 'server_name' == 'orchestra2':
+        resource_bundle_path = '/groups/lpm/erik/WGA/bundle/2.2/b37'
+        tools_dir = '/groups/lpm/erik/WGA/tools'
+
     wga_settings = {
         'GATK_path' :os.path.join(tools_dir,'GenomeAnalysisTKLite-2.2-16-g2cc9ef8/GenomeAnalysisTKLite.jar'),
         'Picard_dir' : os.path.join(tools_dir,'picard-tools-1.78'),
         'bwa_path' : os.path.join(tools_dir,'bwa-0.6.2/bwa'),
-        'bwa_reference_fasta_path' : '/groups/lpm/erik/WGA/bwa_reference/human_g1k_v37.fasta',
+        'bwa_reference_fasta_path' : '/groups/cbi/WGA/bwa_reference/human_g1k_v37.fasta',
         'samtools_path':os.path.join(tools_dir,'samtools-0.1.18/samtools'),
         'get_drmaa_native_specification': get_drmaa_native_specification
         }
@@ -82,6 +87,7 @@ wga_settings.update({
     'hapmap_path' : os.path.join(resource_bundle_path,'hapmap_3.3.b37.vcf'),
     'omni_path' : os.path.join(resource_bundle_path,'1000G_omni2.5.b37.vcf'),
     'mills_path' : os.path.join(resource_bundle_path,'Mills_and_1000G_gold_standard.indels.b37.vcf'),
-    'indels_1000g_phase1_path' : os.path.join(resource_bundle_path,'1000G_phase1.indels.b37.vcf')
+    'indels_1000g_phase1_path' : os.path.join(resource_bundle_path,'1000G_phase1.indels.b37.vcf'),
+    'genomekey_library_path': os.path.dirname(os.path.realpath(__file__))
 })
 
