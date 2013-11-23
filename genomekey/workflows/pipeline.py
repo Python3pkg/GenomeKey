38c52159b4063a18df6553d963f0f75e0ba38c22
Ubuntu <ubuntu@master.(none)>
2013-11-23 07:21:00 +0000

    update /bin/cteamkey

--- genomekey/workflows/pipeline.py
+++ genomekey/workflows/pipeline.py
@@ -1,98 +1,89 @@
-from cosmos.lib.ezflow.dag        import add_, map_, reduce_, split_, reduce_split_, sequence_, apply_
+import os
+import pysam
+#import re
 
-from genomekey.tools              import gatk, picard, bwa, misc, bamUtil, pipes
-from genomekey.workflows.annotate import massive_annotation
+from cosmos.lib.ezflow.dag  import DAG, add_, split_, sequence_, configure, add_run, map_, reduce_, reduce_split_, apply_
+from cosmos.lib.ezflow.tool import INPUT
+from cosmos.Workflow.models import TaskFile
+
+from genomekey.tools              import gatk, picard, pipes
 from genomekey.wga_settings       import wga_settings
 
-def Pipeline2():
-    #is_capture = wga_settings['capture'] => will not reduceReads
+from genomekey       import log
+
+
+class BamException(Exception):pass
+class WorkflowException(Exception):pass
+
+
+def _getHeaderInfo(input_bam):
+    if   input_bam[-3:] == 'bam': header = pysam.Samfile(input_bam,'rb', check_sq = False).header
+    elif input_bam[-3:] == 'sam': header = pysam.Samfile(input_bam,'r' , check_sq = False).header
+    else:
+        raise TypeError, 'input file is not a bam or sam'
+
+    return {'rg': [ [tags['ID'], tags['SM'], tags.get('LB','noLBinfo'), tags.get('PL','noPLinfo') ] for tags in header['RG']],
+            'sq': [ [tags['SN'], tags['LN']]                                                        for tags in header['SQ']]
+           }
+
+def _getSeqName(header):
+    """Return sequence names (@SQ SN in header)
+    """
+
+    seqNameList = []
+    unMapped=''
+    for sn in header['sq']:
+        if (sn[0].startswith('GL')) or (sn[0].startswith('chrUn')):
+            unMapped += " %s" % sn[0]
+        else:
+            seqNameList.append(sn[0])  # first column is seqName
+
+    seqNameList.append(unMapped)
+    return seqNameList
+
+    
+opb  = os.path.basename
+seq_ = sequence_
+
+
+def Pipeline(workflow, dag, settings, bams):
 
     # split_ tuples
     interval = ('interval', range(1,23) + ['X', 'Y'])
 
     glm = ('glm', ['SNP', 'INDEL'])
 
+
+    bam_seq = None
+    
+    for b in bams:
+        header = _getHeaderInfo(b)
+        sn     = _getSeqName(header)
+
+        rgid = [ h[0] for h in header['rg']]
+
+        # if seqName is empty, then let's assume that the input is unaligned bam
+        sample_name = opb(b).partition('.')[0]
+        s = seq_( add_([INPUT(b, tags={'bam':sample_name})], stage_name="Load BAMs"), split_([ ('rgid', rgid), ('sn', sn) ], pipes.Bam_To_BWA))
+
+        if bam_seq is None:   bam_seq = s
+        else:                 bam_seq = seq_(bam_seq, s, combine=True)
+
+
     return sequence_(
+        bam_seq,
+
         reduce_split_(['bam','rgid'], [interval], gatk.IndelRealigner),
+
         map_(picard.MarkDuplicates),
+
         map_(gatk.BaseQualityScoreRecalibration),        
 
         reduce_split_(['bam'],      [interval], gatk.ReduceReads),
+
         reduce_split_(['interval'], [glm],      gatk.UnifiedGenotyper),
 
         reduce_(['glm'],                        gatk.VariantQualityScoreRecalibration, tag={'vcf':'master'}),
-        #map_(gatk.Apply_VQSR, tag={'vcf':'master'}),        
 
-        reduce_(['vcf'], gatk.CombineVariants, "Combine into Master VCFs"),
-
-        massive_annotation
-        )
-
-def Pipeline():
-    is_capture = wga_settings['capture']
-
-    # split_ tuples
-    interval = ('interval', range(1,23) + ['X', 'Y'])
-
-    glm = ('glm', ['SNP', 'INDEL'])
-
-    # sn is seqName, not sample_name
-    # assuming that each data file is unique with rgid+sn, without 'bam' file tag.
-    align = reduce_(['sample_name','rgid','platform','library','sn'], pipes.AlignAndClean) 
-
-    # post_align1 = sequence_(
-    #     reduce_(['sample_name', 'library'], picard.MarkDuplicates),
-    #     split_([interval],gatk.RealignerTargetCreator),
-    #     map_(gatk.IndelRealigner),
-    #     map_(gatk.BQSR),
-    #     map_(gatk.ApplyBQSR) 
-    #     )
-    
-    # post_align2 = sequence_(
-    #     reduce_(['sample_name', 'library'], picard.MarkDuplicates),
-    #     split_([interval], gatk.IndelRealigner),
-    #     map_(gatk.BQSR),
-    #     map_(gatk.ApplyBQSR) 
-    #     )
-
-    # post_align3 = sequence_(
-    #     reduce_split_(['bam','sample_name','library','rgid'],[interval],  gatk.IndelRealigner),
-    #     reduce_(      ['bam','sample_name','library',        'interval'], picard.MarkDuplicates),
-    #     map_(gatk.BQSR),
-    #     map_(gatk.ApplyBQSR)
-    #     )
-
-    post_align = sequence_(
-        reduce_split_(['sample_name', 'library', 'rgid'], [interval], gatk.IndelRealigner),   
-        map_(picard.MarkDuplicates),
-        map_(gatk.BaseQualityScoreRecalibration)
-        )
-
-    call_variants = sequence_(
-#        apply_(
-            #reduce_(['interval'],gatk.HaplotypeCaller,tag={'vcf':'HaplotypeCaller'}),
-#            reduce_split_(['interval'], [glm], gatk.UnifiedGenotyper, tag={'vcf': 'UnifiedGenotyper'}),
-#            combine=True
-#        ),
-        reduce_split_(['interval'], [glm], gatk.UnifiedGenotyper, tag={'vcf': 'UnifiedGenotyper'}),
-        reduce_(['vcf'], gatk.CombineVariants, 'Combine Into Raw VCFs'),
-        split_([glm], gatk.VariantQualityScoreRecalibration),
-        map_(gatk.Apply_VQSR),
         reduce_(['vcf'], gatk.CombineVariants, "Combine into Master VCFs")
-    )
-
-    if is_capture:
-        return sequence_(
-            align,
-            post_align,
-            call_variants,
-            massive_annotation
-        )
-    else:
-        return sequence_(
-            align,
-            post_align,
-            reduce_split_(['sample_name'], [interval], gatk.ReduceReads),
-            call_variants,
-            massive_annotation
         )
