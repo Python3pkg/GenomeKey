9915caa98808cff096bdfb8a2557382bbd29ee06
Ubuntu <ubuntu@master.(none)>
2013-11-23 06:59:38 +0000

    update /workflow directory

--- genomekey/workflows/pipeline.py
+++ genomekey/workflows/pipeline.py
@@ -1,70 +1,98 @@
-from genomekey.tools import gatk, picard, bwa, misc, bamUtil,pipes
-from cosmos.lib.ezflow.dag import add_, map_, reduce_, split_, reduce_split_, sequence_, apply_
-from genomekey.workflows.annotate import massive_annotation
-from genomekey.wga_settings import wga_settings
+from cosmos.lib.ezflow.dag        import add_, map_, reduce_, split_, reduce_split_, sequence_, apply_
 
-def Pipeline():
-    is_capture = wga_settings['capture']
-    testing = wga_settings['test']
+from genomekey.tools              import gatk, picard, bwa, misc, bamUtil, pipes
+from genomekey.workflows.annotate import massive_annotation
+from genomekey.wga_settings       import wga_settings
+
+def Pipeline2():
+    #is_capture = wga_settings['capture'] => will not reduceReads
 
     # split_ tuples
-    if testing:
-        intervals = ('interval', [20])
-    else:
-        intervals = ('interval',range(1,23) + ['X', 'Y'])
+    interval = ('interval', range(1,23) + ['X', 'Y'])
 
     glm = ('glm', ['SNP', 'INDEL'])
 
-    align_to_reference = sequence_(
-        apply_(
-#           reduce_(['sample_name', 'library'], misc.FastqStats),
-            reduce_(['sample_name', 'library', 'platform', 'platform_unit', 'region', 'bam', 'rgid'], pipes.AlignAndClean)
-        ),
-    )
+    return sequence_(
+        reduce_split_(['bam','rgid'], [interval], gatk.IndelRealigner),
+        map_(picard.MarkDuplicates),
+        map_(gatk.BaseQualityScoreRecalibration),        
 
-    preprocess_alignment = sequence_(
-        reduce_(['sample_name', 'library'], picard.MarkDuplicates),
-        apply_(
-#           map_(picard.CollectMultipleMetrics),
-            split_([intervals],gatk.RealignerTargetCreator) #if not is_capture or testing else map_(gatk.RealignerTargetCreator)
-        ),
-        map_(gatk.IndelRealigner),
-        map_(gatk.BQSR),
-        apply_(
-#            reduce_(['sample_name'], gatk.BQSRGatherer),
-            map_(gatk.ApplyBQSR) #TODO I add BQSRGatherer as a parent with a hack inside ApplyBQSR.cmd
+        reduce_split_(['bam'],      [interval], gatk.ReduceReads),
+        reduce_split_(['interval'], [glm],      gatk.UnifiedGenotyper),
+
+        reduce_(['glm'],                        gatk.VariantQualityScoreRecalibration, tag={'vcf':'master'}),
+        #map_(gatk.Apply_VQSR, tag={'vcf':'master'}),        
+
+        reduce_(['vcf'], gatk.CombineVariants, "Combine into Master VCFs"),
+
+        massive_annotation
         )
-    )
+
+def Pipeline():
+    is_capture = wga_settings['capture']
+
+    # split_ tuples
+    interval = ('interval', range(1,23) + ['X', 'Y'])
+
+    glm = ('glm', ['SNP', 'INDEL'])
+
+    # sn is seqName, not sample_name
+    # assuming that each data file is unique with rgid+sn, without 'bam' file tag.
+    align = reduce_(['sample_name','rgid','platform','library','sn'], pipes.AlignAndClean) 
+
+    # post_align1 = sequence_(
+    #     reduce_(['sample_name', 'library'], picard.MarkDuplicates),
+    #     split_([interval],gatk.RealignerTargetCreator),
+    #     map_(gatk.IndelRealigner),
+    #     map_(gatk.BQSR),
+    #     map_(gatk.ApplyBQSR) 
+    #     )
+    
+    # post_align2 = sequence_(
+    #     reduce_(['sample_name', 'library'], picard.MarkDuplicates),
+    #     split_([interval], gatk.IndelRealigner),
+    #     map_(gatk.BQSR),
+    #     map_(gatk.ApplyBQSR) 
+    #     )
+
+    # post_align3 = sequence_(
+    #     reduce_split_(['bam','sample_name','library','rgid'],[interval],  gatk.IndelRealigner),
+    #     reduce_(      ['bam','sample_name','library',        'interval'], picard.MarkDuplicates),
+    #     map_(gatk.BQSR),
+    #     map_(gatk.ApplyBQSR)
+    #     )
+
+    post_align = sequence_(
+        reduce_split_(['sample_name', 'library', 'rgid'], [interval], gatk.IndelRealigner),   
+        map_(picard.MarkDuplicates),
+        map_(gatk.BaseQualityScoreRecalibration)
+        )
 
     call_variants = sequence_(
-        # apply_(
-        #     reduce_split_([],[intervals,glm], gatk.UnifiedGenotyper, tag={'vcf': 'UnifiedGenotyper'}),
-        #     combine=True
-        # ) if is_capture
-        # else
-        apply_(
+#        apply_(
             #reduce_(['interval'],gatk.HaplotypeCaller,tag={'vcf':'HaplotypeCaller'}),
-            reduce_split_(['interval'], [glm], gatk.UnifiedGenotyper, tag={'vcf': 'UnifiedGenotyper'}),
-            combine=True
-        ),
+#            reduce_split_(['interval'], [glm], gatk.UnifiedGenotyper, tag={'vcf': 'UnifiedGenotyper'}),
+#            combine=True
+#        ),
+        reduce_split_(['interval'], [glm], gatk.UnifiedGenotyper, tag={'vcf': 'UnifiedGenotyper'}),
         reduce_(['vcf'], gatk.CombineVariants, 'Combine Into Raw VCFs'),
-        split_([glm],gatk.VQSR),
+        split_([glm], gatk.VariantQualityScoreRecalibration),
         map_(gatk.Apply_VQSR),
         reduce_(['vcf'], gatk.CombineVariants, "Combine into Master VCFs")
     )
 
     if is_capture:
         return sequence_(
-            align_to_reference,
-            preprocess_alignment,
+            align,
+            post_align,
             call_variants,
             massive_annotation
         )
     else:
         return sequence_(
-            align_to_reference,
-            preprocess_alignment,
-            reduce_split_(['sample_name'],[intervals],gatk.ReduceReads),
+            align,
+            post_align,
+            reduce_split_(['sample_name'], [interval], gatk.ReduceReads),
             call_variants,
             massive_annotation
         )
