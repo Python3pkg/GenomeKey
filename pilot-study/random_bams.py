dfdc42ff374ccd0ec270ea2e1ecc35b0e98ca040
Alex Lancaster <alex_lancaster@hms.harvard.edu>
2014-02-12 17:27:13 -0500

    Modify to handle genomes as well.

--- pilot-study/random_bams.py
+++ pilot-study/random_bams.py
@@ -3,10 +3,14 @@
 
 import csv, random, sys
 
+input_file = sys.argv[1]
+
+# should be either "exome" or "genome"
+kind_file = sys.argv[2]
+
 number_bams_to_keep = 100
 number_bams_to_keep_string = str(number_bams_to_keep)
 
-input_file = sys.argv[1]
 output_file_random = number_bams_to_keep_string + '_' + input_file + '_random_bams.index'
 output_file_random_md5sum = number_bams_to_keep_string + '_' + input_file + '_random_bams.md5'
 
@@ -16,9 +20,16 @@ with open(input_file, 'rb') as f:
 	#reader = csv.reader(f)
 	new_rows_list = []
 	for row in reader:
-		if 'mapped' in row[0] and not 'unmapped' in row[0]:
-			new_rows_list.append(row[0])
-			#new_rows_list.append(row[0])
+		if kind_file == 'exome':
+			if 'mapped' in row[0] and not 'unmapped' in row[0]:
+				new_rows_list.append(row[0])
+		elif kind_file == 'genome':
+			# selects only high coverage files ("SRP000032")
+			if 'SRP000032' in row[0] and not 'unmapped' in row[0]:
+				new_rows_list.append(row[0])
+		else:
+			print "unknown file type: must be either 'exome' or 'genome'"
+			exit(-1)
 
 # # write out CSV with only mapped bams
 # with open(output_file_bams, 'wb') as f:
