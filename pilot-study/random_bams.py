0d1d745e08abf16e10249feabedd606de2c865b8
Alex Lancaster <alexlan@fedoraproject.org>
2014-02-12 20:32:50 -0500

    Add optional number of bams and pedigree file to allow output of population info:
    Usage:
    random_bams.py ALIGNMENT_FILE [NUMBAMS] [PEDIGREE]

--- pilot-study/random_bams.py
+++ pilot-study/random_bams.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python
 # simple script to read in csv and output new CSV with random number of bams
+# also, optionally extracts pedigree information for each sample
 
-import csv, random, sys
+import csv, random, sys, os.path
 
 input_file = sys.argv[1]
+basename = os.path.basename(input_file)
 
-# should be either "exome" or "genome"
-kind_file = sys.argv[2]
+# default to 100
+if len(sys.argv) > 2:
+	number_bams_to_keep = int(sys.argv[2])
+else:
+	number_bams_to_keep = 100
 
-number_bams_to_keep = 100
+# default to 100
+if len(sys.argv) > 3:
+	pedigree_file = sys.argv[3]
+else:
+	pedigree_file = False
+
 number_bams_to_keep_string = str(number_bams_to_keep)
 
-output_file_random = number_bams_to_keep_string + '_' + input_file + '_random_bams.index'
-output_file_random_md5sum = number_bams_to_keep_string + '_' + input_file + '_random_bams.md5'
+output_file_random = number_bams_to_keep_string + '_' + basename + '_random_bams.index'
+output_file_random_md5sum = number_bams_to_keep_string + '_' + basename + '_random_bams.md5'
+output_pedigree_filename = number_bams_to_keep_string + '_' + basename + '_g1k.ped'
 
 # read in CSV and save mapped bams to list
 with open(input_file, 'rb') as f:
@@ -20,35 +31,36 @@ with open(input_file, 'rb') as f:
 	#reader = csv.reader(f)
 	new_rows_list = []
 	for row in reader:
-		if kind_file == 'exome':
-			if 'mapped' in row[0] and not 'unmapped' in row[0]:
-				new_rows_list.append(row[0])
-		elif kind_file == 'genome':
-			# selects only high coverage files ("SRP000032")
-			if 'SRP000032' in row[0] and not 'unmapped' in row[0]:
-				new_rows_list.append(row[0])
-		else:
-			print "unknown file type: must be either 'exome' or 'genome'"
-			exit(-1)
+		if 'mapped' in row[0] and not 'unmapped' in row[0]:
+			new_rows_list.append(row[0])
 
-# # write out CSV with only mapped bams
-# with open(output_file_bams, 'wb') as f:
-# 	writer = csv.writer(f)
-# 	writer.writerows(new_rows_list)
-
-# # write out CSV with random number of mapped bams
-# with open(output_file_random, 'wb') as f:
-# 	random_list = random.sample(new_rows_list, number_bams_to_keep)
-# 	writer = csv.writer(f, delimiter='\t')
-# 	#writer = csv.writer(f)
-# 	writer.writerows(random_list)
+if pedigree_file:
+	pedigree_dict = {}
+	with open(pedigree_file, 'rb') as f:
+		reader = csv.reader(f, delimiter='\t')
+		headers = next(reader, None)  # returns the headers or `None` if the input is empty
+		for row in reader:
+			# ID in second column
+			pedigree_dict[row[1]] = row
+	output_pedigree = open(output_pedigree_filename, 'w')
+	output_pedigree.write("\t".join(headers))
+	output_pedigree.write("\n")
+else:
+	pedigree_list = None
 
 # write out text file with random number of mapped bams
 with open(output_file_random, 'wb') as f:
 	random_list = random.sample(new_rows_list, number_bams_to_keep)
 	for bam in random_list:
+		if output_pedigree:
+			# look up info on sample
+			sample_id = bam.split('/')[1]
+			# write back out to file
+			output_pedigree.write("\t".join(pedigree_dict[sample_id]))
+			output_pedigree.write("\n")
 		f.write("%s\n" % bam)
 
-
+if output_pedigree:
+	output_pedigree.close()
 
 
