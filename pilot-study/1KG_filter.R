875985dd3d1be810e36fe4d5d2ef6d1b64e3e7ec
Yassine <yassinesouilmi@gmail.com>
2014-02-19 00:39:54 -0500

    Create 1KG_filter.R
    create the list of exomes file names that meets the criteria: 100x<coverage<120x and European

---
+++ pilot-study/1KG_filter.R
@@ -0,0 +1,27 @@
+## This script reads the information about the 1000 genomes project Exomes from phase I, 
+## give an overview about the MEAN_BAIT_COVERAGE which is the mean coverage of all baits 
+## in the experiment. more: http://picard.sourceforge.net/picard-metric-definitions.shtml#HsMetrics
+
+# this script needs: ftp://ftp-trace.ncbi.nih.gov/1000genomes/ftp/alignment_indices/20130502.exome.alignment.index.HsMetrics.gz
+# the file should be in the working directory, otherwise replace with the absolute PATH
+
+library(lattice)
+
+# loading the data into cvr table
+cvr=read.table("20130502.exome.alignment.index.HsMetrics", header=TRUE, fill=TRUE)
+
+# get some overview and visualization of the distribution of the "mean coverage"
+summary(cvr$MEAN_BAIT_COVERAGE)
+histogram(~MEAN_BAIT_COVERAGE, data=cvr)
+densityplot(~MEAN_BAIT_COVERAGE, data=cvr)
+
+# filter by populations
+# European populations info from: ftp://ftp-trace.ncbi.nih.gov/1000genomes/ftp/README.populations
+sbt = subset(cvr, MEAN_BAIT_COVERAGE > 100 & MEAN_BAIT_COVERAGE < 120 & grepl("CEU|FIN|IBS|GBR|TSI", File_name))
+length(sbt$MEAN_BAIT_COVERAGE)
+
+# get the file names and write to 127_exomes.index
+ex=(sbt$File_name)
+write.table((sbt$File_name), file="127_exomes.index", sep='\n', row.names=FALSE, quote=FALSE)
+
+# note: this write an "x" character in the beginning of the file for some reason I ignore now!
