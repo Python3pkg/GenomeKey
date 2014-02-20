56ba22be7667873b5c4c6e72539514b5e095b655
Alex Lancaster <alex_lancaster@hms.harvard.edu>
2014-02-20 17:01:36 -0500

    Download the metrics file into 1000g for local use. Fix perms and make R script executable.

--- pilot-study/1KG_filter.R
+++ pilot-study/1KG_filter.R
@@ -1,3 +1,4 @@
+#!/usr/bin/env Rscript
 ## This script reads the information about the 1000 genomes project Exomes from phase I, 
 ## give an overview about the MEAN_BAIT_COVERAGE which is the mean coverage of all baits 
 ## in the experiment. more: http://picard.sourceforge.net/picard-metric-definitions.shtml#HsMetrics
@@ -18,10 +19,11 @@ densityplot(~MEAN_BAIT_COVERAGE, data=cvr)
 # filter by populations
 # European populations info from: ftp://ftp-trace.ncbi.nih.gov/1000genomes/ftp/README.populations
 sbt = subset(cvr, MEAN_BAIT_COVERAGE > 100 & MEAN_BAIT_COVERAGE < 120 & grepl("CEU|FIN|IBS|GBR|TSI", File_name))
-length(sbt$MEAN_BAIT_COVERAGE)
+#length(sbt$MEAN_BAIT_COVERAGE)
 
 # get the file names and write to 127_exomes.index
+#head(sbt)
 ex=(sbt$File_name)
-write.table((sbt$File_name), file="127_exomes.index", sep='\n', row.names=FALSE, quote=FALSE)
+write.table((sbt$File_name), file="127_exomes.index", sep='\n', row.names=FALSE, col.names=FALSE quote=FALSE)
 
 # note: this write an "x" character in the beginning of the file for some reason I ignore now!
