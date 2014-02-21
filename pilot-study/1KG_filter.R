a2afdd8db8121229d2f0f41c394f8bf0f27d7927
Alex Lancaster <alex_lancaster@hms.harvard.edu>
2014-02-20 19:48:21 -0500

    Placeholder code for trios. Currently not enough pass the min coverage filter.

--- pilot-study/1KG_filter.R
+++ pilot-study/1KG_filter.R
@@ -11,6 +11,9 @@ library(lattice)
 # loading the data into cvr table
 cvr=read.table("20130502.exome.alignment.index.HsMetrics", header=TRUE, fill=TRUE)
 
+# extract the individual ID
+cvr=transform(cvr, Individual.ID=sub("[.].*", "", cvr$File_name))
+
 # get some overview and visualization of the distribution of the "mean coverage"
 stats = summary(cvr$MEAN_BAIT_COVERAGE)
 li = quantile(cvr$MEAN_BAIT_COVERAGE)
@@ -21,11 +24,27 @@ higher = li["75%"][[1]]
 histogram(~MEAN_BAIT_COVERAGE, data=cvr)
 densityplot(~MEAN_BAIT_COVERAGE, data=cvr)
 
+# load pedigree data
+pedigree=read.table("20130606_g1k.ped", header=T, fill=T, sep="\t")
+
+# merge the datasets
+merged = merge(cvr, pedigree, by="Individual.ID")
+
 # filter by populations
 # European populations info from: ftp://ftp-trace.ncbi.nih.gov/1000genomes/ftp/README.populations
-sbt = subset(cvr, MEAN_BAIT_COVERAGE > lower & MEAN_BAIT_COVERAGE < higher & grepl("CEU|FIN|IBS|GBR|TSI", File_name))
-num.exomes = length(sbt$MEAN_BAIT_COVERAGE)
+pop.regex="CEU|FIN|IBS|GBR|TSI"
 
+# get all individuals with minimum coverage
+min.cov = subset(merged, MEAN_BAIT_COVERAGE > lower & MEAN_BAIT_COVERAGE < higher & grepl(pop.regex, Population))
+
+# now get trios only in the specified population(s)
+# FIXME: disable for the moment
+#trios = subset(min.cov, (Paternal.ID != 0) & (Maternal.ID !=0))
+#min.cov.trios = subset(min.cov, (Individual.ID %in% trios$Paternal.ID) & (Individual.ID %in% trios$Maternal.ID))
+#head(min.cov.trios[,c("Individual.ID", "Paternal.ID", "Maternal.ID", "MEAN_BAIT_COVERAGE")])
+
+num.exomes = length(min.cov$MEAN_BAIT_COVERAGE)
 # get the file names and write to <num.exomes>_exomes.index
-ex=(sbt$File_name)
-write.table((sbt$File_name), file=sprintf("%d_exomes.index", num.exomes), sep='\n', row.names=FALSE, col.names=FALSE, quote=FALSE)
+#write.table(min.cov[,c("File_name", "MEAN_BAIT_COVERAGE", "Paternal.ID", "Maternal.ID")], file=sprintf("%d_exomes.index", num.exomes), row.names=FALSE, col.names=TRUE, quote=FALSE)
+write.table(min.cov[,c("File_name")], file=sprintf("%d_exomes.index", num.exomes), row.names=FALSE, col.names=FALSE, quote=FALSE)
+
