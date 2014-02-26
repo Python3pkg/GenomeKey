321cb48b2d82d2428cbe9508b1b81bb86fb1c5d2
jaeyjung <jae-yoon_jung@med.harvard.edu>
2014-02-26 05:21:01 +0000

    update settings.py to be used across platforms

--- genomekey/settings.py
+++ genomekey/settings.py
@@ -5,29 +5,51 @@ from cosmos.config import settings as cosmos_settings
 
 def _get_drmaa_native_specification(jobAttempt):
     task = jobAttempt.task
+    drm  = cosmos_settings['DRM']
 
-    # did other attempts fail?
-    # is_reattempt = task.jobAttempts.count() >1
-    
     cpu_req  = task.cpu_requirement
     mem_req  = task.memory_requirement
     time_req = task.time_requirement
     queue    = task.workflow.default_queue
-
-    # GridEngine specific option
-    return '-l spock_mem={mem_req}M,num_proc={cpu_req}'.format(mem_req=mem_req, cpu_req=cpu_req)
     
+    if drm == 'LSF':           # for Orchestra Runs
+        if time_req <= 12*60 queue = 'short';
+        else:                queue = 'long';
+                
+        return '-R "rusage[mem={0}] span[hosts=1]" -n {1} -W 0:{2} -q {3}'.format(mem_req, cpu_req, time_req, queue)
+
+    elif drm == 'GE':
+        return '-l spock_mem={mem_req}M,num_proc={cpu_req}'.format(mem_req=mem_req, cpu_req=cpu_req)
+
+    else:
+        raise Exception('DRM not supported')
+
+
+svr  = cosmos_settings['server_name']
+
+if svr == 'orchestra':
+      ref_path = '/groups/cbi/WGA/reference'
+    tools_path = '/groups/cbi/WGA/tools'
+
+elif svr == 'aws':
+      ref_path = '/WGA/reference'
+    tools_path = '/WGA/tools'
+
+elif svr == 'gce':
+      ref_path = '/pseq/WGA/ref'  # In shared disk
+    tools_path = '/tools/'        # In boot   disk
+else:
+    raise Exception('Unknown server_name {0} in Cosmos configuration: must be one of [orchestra, aws, gce]'.format(svr))
+
 
 opj = os.path.join
 
-
-ref_path = '/pseq/WGA/ref'  # In shared disk
-tools_path='/tools/'        # In boot   disk
 
 settings = {
     'date'                  : '/bin/date "+%Y-%m-%d %H:%M:%S"',  
     'java'                  : 'java -d64 -XX:ParallelGCThreads=2 -XX:+UseParallelOldGC -XX:+AggressiveOpts',
     'scratch'               : '/scratch',
+
     'htscmd'                : opj(tools_path, 'htscmd'),
     'bwa'                   : opj(tools_path, 'bwa'),              
     'gatk'                  : opj(tools_path, 'gatk.jar'),
