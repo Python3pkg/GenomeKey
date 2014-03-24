637c006f5b5499bc265ba1ac2e49225b784c9ae6
Alex Lancaster <alex_lancaster@hms.harvard.edu>
2014-03-24 17:55:56 -0400

    Some notes, fix quoting.

--- pilot-study/CosmosRes.sh
+++ pilot-study/CosmosRes.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
+# FIXME: use variable names, e.g. LIST=$1 etc.
 # $1:: list
 # $2:: output bucket
 # $3:: port
 # (respectively name, user and password::$4 $5 $6)
 # $7:: cosmos paths (temp and output)
-# $8:: server name
+# $8:: root of scratch directory
+# $9:: email address
+# $10:: extra args to genomekey  (give an empty string if none needed)
 
 # Notify the user of the start
 
-echo "Genomekey run `$1` started" | mail -s "GenomeKey `$1` run Started" "$9"
+echo "Genomekey run \"$1\" started" | mail -s "GenomeKey \"$1\" run Started" "$9"
 
 # Step 0) Make an output dir
 
-mkdir $8/Out/"$1"
+mkdir -p $8/Out/"$1"
 
 # Step 1) Launch the run
 
-genomekey bam -n "$1" -il $1  #### Here we still need to test if the run was successful or not
+genomekey bam -n "$1" -il $1 $10  #### Here we still need to test if the run was successful or not
 
 if [$? -eq 0]; then
 
@@ -40,6 +43,7 @@ if [$? -eq 0]; then
       aws s3 cp $8/cosmos/out/stage_name/.../annotated.vcf $2/"$1"/
 
     # Step 5) Clean-up
+    # FIXME: figure out how to make this generic
     if [$8 -eq orchestra]; then
         rm -R $8/cosmos/out/*
         rm -R $8/cosmos/tmp/*
@@ -49,7 +53,7 @@ if [$? -eq 0]; then
     fi
 
     rm $8/Out/*
-    echo "Genomekey run `$1` data successfully backup on S3" | mail -s "GenomeKey Backup" "$9"
+    echo "Genomekey run \"$1\" data successfully backup on S3" | mail -s "GenomeKey Backup" "$9"
 else
-    echo "Genomekey run `$1` failed" | mail -s "GenomeKey run Failure" "$9"
+    echo "Genomekey run \"$1\" failed" | mail -s "GenomeKey run Failure" "$9"
 fi
