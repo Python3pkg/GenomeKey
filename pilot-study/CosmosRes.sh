608b7f6ada5b91b4454b059a17e279e1b801028a
yassineS <yassinesouilmi@gmail.com>
2014-03-25 14:16:07 -0400

    Untested: This update fixes the issues reported by Alex yesterday

--- pilot-study/CosmosRes.sh
+++ pilot-study/CosmosRes.sh
@@ -1,59 +1,58 @@
 #!/bin/bash
 
-# FIXME: use variable names, e.g. LIST=$1 etc.
-# $1:: list
-# $2:: output bucket
-# $3:: port
-# (respectively name, user and password::$4 $5 $6)
-# $7:: cosmos paths (temp and output)
-# $8:: root of scratch directory
-# $9:: email address
-# $10:: extra args to genomekey  (give an empty string if none needed)
+List=$1  # list
+OutBucket=$2  # output bucket
+DBname=$DBname
+DBuser=$4
+DBpasswd=$5    # (rectively name, user and password::$3 $4 $5)
+default_root_output_dir=$6  # root of scratch directory; cosmos paths (temp and output)
+working_directory=$7
+Email=$8  # email address
+GKargs=$9 # extra args to genomekey  (give an empty string if none needed)
 
 # Notify the user of the start
 
-echo "Genomekey run \"$1\" started" | mail -s "GenomeKey \"$1\" run Started" "$9"
+echo "Genomekey run \"$List\" started" | mail -s "GenomeKey \"$List\" run Started" "$Email"
 
 # Step 0) Make an output dir
 
-mkdir -p $8/Out/"$1"
+mkdir -p $HOME/Out/"$List"
 
 # Step 1) Launch the run
 
-genomekey bam -n "$1" -il $1 ${10}  #### Here we still need to test if the run was successful or not
+genomekey bam -n "$List" -il $List ${Gkargs}  #### Here we still need to test if the run was successful or not
 
 if [ $? -eq 0 ]; then
 
-    echo "Genomekey run \"$1\" was successful" | mail -s "GenomeKey run Successful" "$9"
-   
+    echo "Genomekey run \"$List\" was successful" | mail -s "GenomeKey run Successful" "$Email"
+
     # Step 2) Dump the DB (the username and the password are hard-coded here)
 
-    mysqldump -u $5 -p $6 –no-create-info $4 > $8/Out/"$1".sql
+    mysqldump -u $DBuser -p $DBpasswd --no-create-info $DBname > $HOME/Out/"$List".sql
 
     # Step 3) Reset cosmos DB
 
     cosmos resetdb
 
-    # Step 4) Copy files to S3
+    # Step 3) Copy files to S3
 
       #cp the DB
-      aws s3 cp $8/Out/"$1"/ $2/"$1"/
+      aws s3 cp $HOME/Out/"$List".sql $OutBucket/"$List"/
 
-      #cp the VCF
-      aws s3 cp $8/cosmos/out/stage_name/.../annotated.vcf $2/"$1"/
+      #cp the BAM after BQSR
+      aws s3 cp $default_root_output_dir/"$List"/BQSR/ $OutBucket/"$List"/ --recursive
 
-    # Step 5) Clean-up
-    # FIXME: figure out how to make this generic
-    if [$8 -eq orchestra]; then
-        rm -R $8/cosmos/out/*
-        rm -R $8/cosmos/tmp/*
-    else
-        rm -R $8/cosmos_out/*
-        rm -R $8/cosmos/erik/*
-    fi
+      #cp the gVCF
+      aws s3 cp $default_root_output_dir/"$List"/HaplotypeCaller/ $OutBucket/"$List"/ --recursive
 
-    rm $8/Out/*
-    echo "Genomekey run \"$1\" data successfully backup on S3" | mail -s "GenomeKey Backup" "$9"
+      #cp the masterVCF
+      # aws s3 cp $default_root_output_dir/"$List"/MasterVCF/ $OutBucket/"$List"/ --recursive #stage not available yet
+
+    # Step 3) Clean-up
+    rm -R -f $default_root_output_dir/*
+    rm -R -f $working_directory/*
+
+    echo "Genomekey run \"$List\" data successfully backup on S3" | mail -s "GenomeKey Backup" "$Email"
 else
-    echo "Genomekey run \"$1\" failed" | mail -s "GenomeKey run Failure" "$9"
+    echo "Genomekey run \"$List\" failed" | mail -s "GenomeKey run Failure" "$Email"
 fi
--- pilot-study/w.sh
+++ pilot-study/w.sh
@@ -12,42 +12,28 @@
 # > ./w.sh lists s3://COSMOS_Pilot/Out/ port eg@email.com
 #
 ##################
-# $1: list of lists
-# $2 output bucket
-# $3 port
-# $4 email address
+Lists=$1     # $1: list of lists
+OutBucket=$2 # $2 output bucket.. In the pilot study it's s3://COSMOS_Pilot/Out/
+Port=$3      # $3 port
+Email=$4     # $4 email address
+GKargs=$5    # $5 extra args for genomekey
+
+if [ $# -eq 4 ]; then
+    Gkargs=""
+fi
 
 #change permissions on the files
 chmod +x ./CosmosRes.sh
 #getting the DB info from the Cosmos config file
 ~/.cosmos/ config.ini
 
-################
-# Defining working directory path
-if [$server_name -eq orchestra]; then
-    scratch=/hms/scratch1/"$USER"/
-
-else
-    scratch=/gluster/gv0/
-
-fi
-
-# Output directory
-OutDir=$scratch/out/
-
-if [ -d $directory ]; then
-  echo "Directory exists"
-else
-  echo "Creating Dirctory"
-  mkdir $scratch/out/
-fi
 
 ################
 # Starting the run
 
 while read F
 do
-./CosmosRes.sh $F $2 $3 $NAME $USER $PASSWORD $server_name $scratch $4
+./CosmosRes.sh $F $OutBucket $NAME $USER $PASSWORD $default_root_output_dir $working_directory $Email $GKargs
 done <$1
 
 ################
