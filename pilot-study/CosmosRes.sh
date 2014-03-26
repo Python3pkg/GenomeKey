06bced1e3ccd3646cc73784acbdc5ff45a9cda10
yassineS <yassinesouilmi@gmail.com>
2014-03-26 18:41:59 -0400

    fixing the automation script errors reported 25/03/14

--- pilot-study/CosmosRes.sh
+++ pilot-study/CosmosRes.sh
@@ -3,60 +3,68 @@
 # Author: Yassine Souilmi
 # March 2014
 
-LIST=$1  # list
+S3LIST=$1  # list
 OUTBUCKET=$2                       # output AWS S3 bucket
 DBNAME=$3
 DBUSER=$4
-DBPASSWD=$5    
+DBPASSWD=$5
 COSMOS_DEFAULT_ROOT_OUTPUT_DIR=$6  # root of scratch directory; COSMOS paths (temp and output)
 COSMOS_WORKING_DIRECTORY=$7        # COSMOS working directory
 EMAIL=$8                           # email address to send report
 GK_ARGS=$9                         # extra args to GenomeKey  (give an empty string if none needed)
 
+# Download the data from S3
+mkdir -p ${COSMOS_WORKING_DIRECTORY}/"${S3LIST}"/Inputs/ #making an Input directory under ${COSMOS_WORKING_DIRECTORY}
+
+while read F
+do
+aws s3 cp $F ${COSMOS_WORKING_DIRECTORY}/"${S3LIST}"/Inputs/
+done <$LISTS
+
+ls ${COSMOS_WORKING_DIRECTORY}/"${S3LIST}"/Inputs/ >> ${COSMOS_WORKING_DIRECTORY}/"${S3LIST}"/Inputs/"${S3LIST}".idx #creating the local list
+
+LIST=${COSMOS_WORKING_DIRECTORY}/"${S3LIST}"/Inputs/"${S3LIST}".idx
+
 # Notify the user of the start
 
-echo "GenomeKey run \"${LIST}\" started" | mail -s "GenomeKey \"${LIST}\" run Started" "${EMAIL}"
+echo "GenomeKey run \"${S3LIST}\" started" | mail -s "GenomeKey \"${S3LIST}\" run Started" "${EMAIL}"
 
-# Step 0) Make an output dir
-# FIXME: remove
-#mkdir -p $HOME/Out/"${LIST}"
+# Step 2) Launch the run
 
-# Step 1) Launch the run
-
-genomekey bam -n "${LIST}" -il ${LIST} ${GK_ARGS}  #### Here we still need to test if the run was successful or not
+genomekey bam -n "${S3LIST}" -il ${LIST} ${GK_ARGS}  #### Here we still need to test if the run was successful or not
 
 if [ $? -eq 0 ]; then
 
-    echo "GenomeKey run \"${LIST}\" was successful" | mail -s "GenomeKey run Successful" "${EMAIL}"
+    echo "GenomeKey run \"${S3LIST}\" was successful" | mail -s "GenomeKey run Successful" "${EMAIL}"
 
-    # Step 2) Dump the DB 
-    cmd="mysqldump -u ${DBUSER} -p${DBPASSWD} --no-create-info ${DBNAME} > ${COSMOS_WORKING_DIRECTORY}/\"${LIST}\".sql"
+    # Dump the DB
+    cmd="mysqldump -u ${DBUSER} -p${DBPASSWD} --no-create-info ${DBNAME} > ${COSMOS_WORKING_DIRECTORY}/\"${S3LIST}\".sql"
     echo $cmd
     eval $cmd
 
-    # Step 3) Reset cosmos DB
-    # FIXME: need to force a reset by overriding the interactive yes/no prompt
-    cosmos resetdb
+    # Reset cosmos DB
 
-    # Step 3) Copy files to S3
+    yes | cosmos resetdb
+
+    # Copy files to S3
 
     #cp the MySQL DB
-    aws s3 cp ${COSMOS_WORKING_DIRECTORY}/"${LIST}".sql ${OUTBUCKET}"${LIST}"/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+    aws s3 cp ${COSMOS_WORKING_DIRECTORY}/"${S3LIST}".sql ${OUTBUCKET}"${LIST}"/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 
     #cp the BAM after BQSR
-    aws s3 cp ${COSMOS_DEFAULT_ROOT_OUTPUT_DIR}/"${LIST}"/BQSR/ ${OUTBUCKET}"${LIST}"/BQSR/ --recursive --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+    aws s3 cp ${COSMOS_DEFAULT_ROOT_OUTPUT_DIR}/"${S3LIST}"/BQSR/ ${OUTBUCKET}"${S3LIST}"/BQSR/ --recursive --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 
     #cp the gVCF
-    aws s3 cp ${COSMOS_DEFAULT_ROOT_OUTPUT_DIR}/"${LIST}"/HaplotypeCaller/ ${OUTBUCKET}"${LIST}"/HaplotypeCaller/ --recursive --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+    aws s3 cp ${COSMOS_DEFAULT_ROOT_OUTPUT_DIR}/"${S3LIST}"/HaplotypeCaller/ ${OUTBUCKET}"${S3LIST}"/HaplotypeCaller/ --recursive --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 
     #cp the masterVCF
     # aws s3 cp $default_root_output_dir/"${LIST}"/MasterVCF/ ${OUTBUCKET}"${LIST}"/ --recursive #stage not available yet
 
-    # Step 3) Clean-up
+    # Clean-up
     rm -R -f ${COSMOS_DEFAULT_ROOT_OUTPUT_DIR}/*
     rm -R -f ${COSMOS_WORKING_DIRECTORY}/*
 
-    echo "Genomekey run \"${LIST}\" data successfully backup on S3" | mail -s "GenomeKey Backup" "${EMAIL}"
+    echo "Genomekey run \"${S3LIST}\" data successfully backup on S3" | mail -s "GenomeKey Backup" "${EMAIL}"
 else
-    echo "Genomekey run \"${LIST}\" failed" | mail -s "GenomeKey run Failure" "${EMAIL}"
+    echo "Genomekey run \"${S3LIST}\" failed" | mail -s "GenomeKey run Failure" "${EMAIL}"
 fi
--- pilot-study/w.sh
+++ pilot-study/w.sh
@@ -12,14 +12,14 @@
 # > ./w.sh lists s3://COSMOS_Pilot/Out/ port eg@email.com
 #
 ##################
-Lists=$1     # $1: list of lists
-OutBucket=$2 # $2 output bucket.. In the pilot study it's s3://COSMOS_Pilot/Out/
-Port=$3      # $3 port
-Email=$4     # $4 email address
-GKargs=$5    # $5 extra args for genomekey
+LISTS=$1     # $1: list of lists
+OUTBUCKET=$2 # $2 output bucket.. In the pilot study it's s3://COSMOS_Pilot/Out/
+PORT=$3      # $3 port
+EMAIL=$4     # $4 email address
+GKARGS=$5    # $5 extra args for genomekey
 
 if [ $# -eq 4 ]; then
-    Gkargs=""
+    GKARGS=""
 fi
 
 #change permissions on the files
@@ -27,13 +27,17 @@ chmod +x ./CosmosRes.sh
 #getting the DB info from the Cosmos config file
 ~/.cosmos/ config.ini
 
+################
+#Launching the webserver
+
+cosmos runweb -p $PORT
 
 ################
 # Starting the run
 
 while read F
 do
-./CosmosRes.sh $F $OutBucket $NAME $USER $PASSWORD $default_root_output_dir $working_directory $Email $GKargs
-done <$1
+./CosmosRes.sh $F $OUTBUCKET $NAME $USER $PASSWORD $default_root_output_dir $working_directory $EMAIL $GKARGS
+done <$LISTS
 
 ################
