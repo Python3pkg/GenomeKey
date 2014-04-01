871ca9796fc4e11eacc32220674fd5aa3626eb5d
Alex Lancaster <alexlan[AT]fedoraproject org>
2014-04-01 15:42:26 -0400

    Add a script to generate tiny BAM takes an arbitrary number and regenerates BAMs with unique RG and SM tags.

---
+++ pilot-study/generate_tiny_bams.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# generate an arbitrary number BAM files from an input BAM
+# with different RG (read group) and SM (sample tags)
+# Usage:
+# generate_tiny_bams.sh <num-bams> <source.bam>
+
+
+END=$1
+INPUT_BAM=$2
+
+OUTPUT_BAM_BASENAME=$(basename ${INPUT_BAM})
+OUTPUT_BAM_PREFIX=${OUTPUT_BAM_BASENAME%.bam}
+
+# get unique "SM" (sample) tag to rewrite
+ID=$(samtools view -H ${INPUT_BAM} |grep "SM:"|cut -f3|cut -d':' -f2|sort|uniq)
+
+for i in $(seq 1 $END)
+do 
+    # zero-pad output
+    printf -v j "%02d" $i
+    cmd="samtools view -H ${INPUT_BAM} | sed \"s/${ID}/${ID}-${j}/g\" | samtools reheader - ${INPUT_BAM} > ${OUTPUT_BAM_PREFIX}-${j}.bam"
+    echo $cmd
+    eval $cmd
+done
+
+
+
