9625756739f90a61aca1c61f17bc89870c14e7f1
Alexander K Lancaster <alex_lancaster@hms.harvard.edu>
2014-04-02 02:51:43 -0400

    Use picard AddReplaceReadGroup to generate BAMs. Need to change the read info as well, not just header, also generate indexes and add to git

--- pilot-study/generate_tiny_bams.sh
+++ pilot-study/generate_tiny_bams.sh
@@ -18,7 +18,12 @@ for i in $(seq 1 $END)
 do 
     # zero-pad output
     printf -v j "%02d" $i
-    cmd="samtools view -H ${INPUT_BAM} | sed \"s/${ID}/${j}/g\" | samtools reheader - ${INPUT_BAM} > tinytestbam-${j}.bam"
+    #cmd="samtools view -H ${INPUT_BAM} | sed \"s/${ID}/${j}/g\" | samtools reheader - ${INPUT_BAM} > tinytestbam-${j}.bam"
+    OUTPUT_BAM=tinytestbam-${j}.bam
+    cmd="java -jar /groups/cbi/WGA/tools/picard-tools-1.99/AddOrReplaceReadGroups.jar I=${INPUT_BAM} O=${OUTPUT_BAM}  SORT_ORDER=coordinate RGPU=barcode RGID=${j} RGLB=${j} RGPL=Illumina RGSM=${j} CREATE_INDEX=True VALIDATION_STRINGENCY=LENIENT"
+    echo $cmd
+    eval $cmd
+    cmd="/groups/cbi/WGA/tools/samtools index ${OUTPUT_BAM}"
     echo $cmd
     eval $cmd
 done
