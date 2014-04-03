ffb2cb3e4fba1d399a66b85d02fe3460997ba9c9
Alexander K Lancaster <alex_lancaster@hms.harvard.edu>
2014-04-03 17:10:15 -0400

    Fix syntax errors. Use ${} consistently throughout. CosmosRes -> CosmosReset Output all command-line args Remove 'exit' which was only for debugging.

--- pilot-study/automation.sh
+++ pilot-study/automation.sh
@@ -9,48 +9,46 @@
 #
 ##################
 ###Example:
-# > ./w.sh lists s3://COSMOS_Pilot/Out/ port eg@email.com
+# > ./automation.sh lists s3://COSMOS_Pilot/Out/ port eg@email.com
 #
 ##################
 LISTS=$1     # $1: list of lists
 OUTBUCKET=$2 # $2 output bucket.. In the pilot study it's s3://COSMOS_Pilot/Out/
 PORT=$3      # $3 port
 EMAIL=$4     # $4 email address
-GKARGS=$5    # $5 extra args for genomekey
+GK_ARGS=$5    # $5 extra args for genomekey
 GK_PATH=$6   # $6 the full path to the GenomeKey installation directory
 
 ##################
 if [ $# -eq 4 ]; then
-    GKARGS=""
+    GK_ARGS=""
 fi
 
 ################
 #change permissions on the files
-chmod +x ${GK_PATH}/CosmosRes.sh
+chmod +x ${GK_PATH}/pilot-study/CosmosReset.sh
 #getting the DB info from the Cosmos config file
 source $HOME/.cosmos/config.ini
 
 #getting the tools path from the GenomeKey settings
 #source ${GK_PATH}/genomekey/settings.py
-if [$server_name -eq "orchestra"]; then
+if [ "${server_name} == "orchestra" ]; then
 
     TOOLS_PATH=/groups/cbi/WGA/tools
 
-  elif [$server_name -eq "aws"]; then
+  elif [ "${server_name} == "aws" ]; then
 
     TOOLS_PATH=/WGA/tools
 fi
 
-echo $NAME $USER $PASSWORD $default_root_output_dir $working_directory ${EMAIL} $TOOLS_PATH
-
-exit
+echo ${NAME} ${USER} ${PASSWORD} ${default_root_output_dir} ${working_directory} ${EMAIL} ${TOOLS_PATH} ${GK_PATH} ${GK_ARGS}
 
 ################
 # Starting the run
 
 while read F
 do
-./CosmosRes.sh $F ${OUTBUCKET} $NAME $USER $PASSWORD $default_root_output_dir $working_directory ${EMAIL} ${GKARGS} ${GK_PATH} $TOOLS_PATH
+${GK_PATH}/pilot-study/CosmosReset.sh $F ${OUTBUCKET} ${NAME} ${USER} ${PASSWORD} ${default_root_output_dir} ${working_directory} ${EMAIL} ${GK_ARGS} ${GK_PATH} ${TOOLS_PATH}
 done <${LISTS}
 
 ################
