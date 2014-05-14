1d9527e1a403f1c63f7c0c7ed92bbe13ae915a1a
Ubuntu <yassinesouilmi@gmail.com>
2014-05-14 01:44:52 +0000

    fixing the ssh commands in the compute nodes

--- pilot-study/init-glusterfs.sh
+++ pilot-study/init-glusterfs.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-# Make sure /mnt was correctly setup before running this script
 
 GLUSTER_VOLUME=$1
-NUMBER_OF_NODES=$2
+
+# Make sure /mnt was correctly setup before running this script
 
 if [ `hostname` == "master" ]; then
-    for node in `cat /etc/hosts | awk '{print $1}'`; do
-        gluster peer probe "$node"
+    for node in `cat /etc/hosts | awk '{print $1}'`; do 
+	gluster peer probe "$node"
     done
-    sudo mkdir -p /mnt/"${GLUSTER_VOLUME}"1
     
-    # ssh "node001" "sudo mkdir -p /mnt/gv2"   # if you want to add this storage to the volume
+      sudo mkdir -pv /mnt/$GLUSTER_VOLUME
+
+         # ssh "node001" "sudo mkdir -p /mnt/gv2"   # if you want to add this storage to the volume
+      sudo gluster volume create $GLUSTER_VOLUME master:/mnt/$GLUSTER_VOLUME
     
-    sudo gluster volume create ${GLUSTER_VOLUME} master:/mnt/"${GLUSTER_VOLUME}"1
-    
-    # sudo gluster volume create ${GLUSTER_VOLUME} master:/mnt/gv1 node001:/mnt/gv2 # if you want to add master:/gv0 AND node001:/gv1
-    
-    sleep 10
-    
-    sudo gluster volume start  ${GLUSTER_VOLUME}
+	# sudo gluster volume create $GLUSTER_VOLUME master:/mnt/$GLUSTER_VOLUME node001:/mnt/$GLUSTER_VOLUME # if you want to add master:/$GLUSTER_VOLUME AND node001:/$GLUSTER_VOLUME
+      
+      sleep 1
+      sudo gluster volume start  $GLUSTER_VOLUME
 fi
 
 
 # Do this here to save some time for setting up glusterfs in the master node
-for node in `cat /etc/hosts | awk '{print $1}'`
-do
-    ssh "$i" "sudo mkdir -p /gluster/${GLUSTER_VOLUME} && sudo mount -t glusterfs master:/${GLUSTER_VOLUME} /gluster/${GLUSTER_VOLUME}"
-    ssh "$i" "sudo chown -R ubuntu:ubuntu /gluster/${GLUSTER_VOLUME}"
-done
+if [ `hostname` == "master" ]; then
+    for node in `cat /etc/hosts | awk '{print $1}'`; do
+         ssh "$node" "mkdir -pv /gluster/$GLUSTER_VOLUME && sudo mount -t glusterfs master:/$GLUSTER_VOLUME /gluster/$GLUSTER_VOLUME"
+         ssh "$node" "chown -R ubuntu:ubuntu /gluster/$GLUSTER_VOLUME"
 
-echo "gluster volume: $GLUSTER_VOLUME created"
-echo "gluster volume mounted on all the $NUMBER_OF_NODES compute nodes"
+         echo $node
+         df -h | grep scratch
+    done
+fi
