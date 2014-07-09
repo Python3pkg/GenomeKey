932ec3e45ca5b734e87027571382cfed3f30b27f
Ubuntu <yassinesouilmi@gmail.com>
2014-07-09 15:26:48 +0000

    Improving the monitor script

--- pilot-study/monitorGK.sh
+++ pilot-study/monitorGK.sh
@@ -8,7 +8,9 @@
 
 LOGFILE=$1 #$1: path to the Genomekey log file
 
-total_submitted=$(grep "Submitted" $LOGFILE|wc -l)
+df -h |grep "scratch"
+echo " "
+total_submittied=$(grep "Submitted" $LOGFILE|wc -l)
 total_successful=$(grep "successful" $LOGFILE|wc -l)
 total_failed=$(grep "failed" $LOGFILE|wc -l)
 echo "total submitted jobs: $total_submitted"
@@ -21,62 +23,56 @@ successful=$(grep "BWA" $LOGFILE|grep "successful"|wc -l)
 echo "Number of successful Jobs: $successful"
 queuing=$(qstat|grep "BWA"|wc -l)
 running=$(qstat -s r|grep "BWA"|wc -l)
-echo "Number of Jobs in the queue ${queuing}, whith $queuing running"
-echo ""
+echo "Number of Jobs in the queue ${queuing}, whith $running running"
 echo "***********"
-echo ""
 
 echo "*** Stage 2: IndelRealigner"
 successful=$(grep "Indel" $LOGFILE|grep "successful"|wc -l)
 echo "Number of successful Jobs: $successful"
 queuing=$(qstat|grep "Indel"|wc -l)
 running=$(qstat -s r|grep "Indel"|wc -l)
-echo "Number of Jobs in the queue ${queuing}, whith $queuing running"
-echo ""
+echo "Number of Jobs in the queue ${queuing}, whith $running running"
 echo "***********"
-echo ""
-
 
 echo "*** Stage 3: MarkDuplicates"
 successful=$(grep "Mark" $LOGFILE|grep "successful"|wc -l)
 echo "Number of successful Jobs: $successful"
 queuing=$(qstat|grep "Mark"|wc -l)
 running=$(qstat -s r|grep "Mark"|wc -l)
-echo "Number of Jobs in the queue ${queuing}, whith $queuing running"
-echo ""
+echo "Number of Jobs in the queue ${queuing}, whith $running running"
 echo "***********"
-echo ""
 
+echo "*** Stage 4: BQSR"
+successful=$(grep "BQSR" $LOGFILE|grep "successful"|wc -l)
+echo "Number of successful Jobs: $successful"
+queuing=$(qstat|grep "BQSR"|wc -l)
+running=$(qstat -s r|grep "BQSR"|wc -l)
+echo "Number of Jobs in the queue ${queuing}, whith $running running"
+echo "***********"
 
-echo "*** Stage 4: HaplotypeCaller"
+echo "*** Stage 5: HaplotypeCaller"
 successful=$(grep "Hap" $LOGFILE|grep "successful"|wc -l)
 echo "Number of successful Jobs: $successful"
 queuing=$(qstat|grep "Hap"|wc -l)
 running=$(qstat -s r|grep "Hap"|wc -l)
-echo "Number of Jobs in the queue ${queuing}, whith $queuing running"
-echo ""
+echo "Number of Jobs in the queue ${queuing}, whith $running running"
 echo "***********"
-echo ""
 
-echo "*** Stage 4: GenotypeGVF"
+echo "*** Stage 6: GenotypeGVF"
 successful=$(grep "Genotype" $LOGFILE|grep "successful"|wc -l)
 echo "Number of successful Jobs: $successful"
 queuing=$(qstat|grep "Genotype"|wc -l)
 running=$(qstat -s r|grep "Genotype"|wc -l)
-echo "Number of Jobs in the queue ${queuing}, whith $queuing running"
-echo ""
+echo "Number of Jobs in the queue ${queuing}, whith $running running"
 echo "***********"
-echo ""
 
-echo "*** Stage 4: VQSR"
+echo "*** Stage 7: VQSR"
 successful=$(grep "VQSR" $LOGFILE|grep "successful"|wc -l)
 echo "Number of successful Jobs: $successful"
 queuing=$(qstat|grep "VQSR"|wc -l)
 running=$(qstat -s r|grep "VQSR"|wc -l)
-echo "Number of Jobs in the queue ${queuing}, whith $queuing running"
-echo ""
+echo "Number of Jobs in the queue ${queuing}, whith $running running"
 echo "***********"
-echo ""
 
 
 
